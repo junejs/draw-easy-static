e7e8bdacbb62a829eadcbc58ea04572c4ff48d17
zhangjun3 <zhangjun@hxmec.com>
2020-03-01 23:46:05 +0800

    experimemt

---
+++ src/components/DirTree.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <el-tree :data="tree" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    </div>
+</template>
+<script>
+export default {
+    name: "DirTree",
+    data() {
+        return {
+            tree: [
+                {
+                    label: "一级1",
+                    id: "1",
+                    children: [
+                        {
+                            label: "二级1-2",
+                            id: "1-2"
+                        }
+                    ]
+                },
+                {
+                    label: "一级2",
+                    id: "2",
+                    children: [
+                        {
+                            label: "二级2-2",
+                            id: "2-2"
+                        }
+                    ]
+                },
+            ],
+            defaultProps: {
+                children: 'children',
+                label: 'label'
+            }
+        }
+    },
+    methods: {
+        handleNodeClick(data) {
+            console.log(data)
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<el-container style="height: 100%">
+			<!-- 左边栏 -->
+			<el-aside style="border: solid 1px red;">
+				<dir-tree></dir-tree>
+			</el-aside>
+			<!-- 右侧 -->
+			<el-container>
+				<!-- 头 -->
+				<el-header>
+					<el-row>
+						<el-col :span="12">
+							<el-input placeholder="搜索标题/创建人" prefix-icon="el-icon-search"></el-input>
+						</el-col>
+						<el-col :span="12">
+							<span>登陆人信息</span>
+						</el-col>
+					</el-row>
+				</el-header>
+				<!-- 主 -->
+				<el-main>
+					<p>main</p>
+				</el-main>
+			</el-container>
+		</el-container>
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DirTree from "@/components/DirTree.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home",
+	components: {
+		DirTree
+	}
+};
 </script>
+<style scoped>
+.home {
+	height: 100%;
+}
+</style>
